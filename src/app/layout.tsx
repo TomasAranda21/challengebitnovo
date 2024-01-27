01255a719f5ea451ab4361a326b2d51d53b89dfe
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-26 23:56:15 -0300

    Add footer component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Footer } from "./ui/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex h-screen w-full flex-col items-center justify-center ">
+          {children}
+
+        </div>
+        <Footer/>
+      </body>
     </html>
   );
 }
