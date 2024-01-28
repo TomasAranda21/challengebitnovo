adf42c15cb672f4cbab5884422e763b88d203c78
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-27 23:20:02 -0300

    Refactor Spinner component to accept size prop

--- src/app/ui/components/Spinner.tsx
+++ src/app/ui/components/Spinner.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export const Spinner = () => {
+export const Spinner = ({size} : {size?: string}) => {
+    const xl = `w-[80px] h-[80px] border-[7px]`
+    const sm = `w-6 h-6 border-[3px]`
+
     return (
         <div className="flex-col gap-4 w-full flex items-center justify-center">
-            <div className="w-6 h-6 border-[3px] text-blue-400 text-4xl animate-spin
+            <div className={`${size !== 'xl' ? sm : xl} text-blue-400 text-4xl animate-spin
              border-gray-300 flex items-center justify-center 
-             border-t-blue-400 rounded-full">
+             border-t-blue-400 rounded-full`}>
             </div>
         </div>
     )
