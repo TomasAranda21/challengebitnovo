96e0c0fc0ce279a72d9bdd8d8c1cd9547a4dee36
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-27 21:55:46 -0300

    Add Footer component

---
+++ src/app/ui/components/Footer.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className='text-center flex items-center justify-center pb-10'>
+      <Image src='/imageFooter.png' className='px-4' width={164} height={26} alt="imageFooter" />
+      <p className='text-sm text-gray-400 border-gray-400 border-l-[1.4px] px-4'>© 2022 Bitnovo. All rights reserved.</p>
+    </footer>
+  )
+}
