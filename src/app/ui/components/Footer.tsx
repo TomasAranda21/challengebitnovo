09247d3a1d41fc3f30fb3c8a72db6908e150bf1d
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-29 19:45:31 -0300

    Update Footer styling in Footer.tsx

--- src/app/ui/components/Footer.tsx
+++ src/app/ui/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className='text-center flex items-center justify-center pb-10 mt-5 lg:mt-0'>
+    <footer className='text-center flex items-center justify-center py-5 lg:pt-0'>
       <Image src='/imageFooter.png' className='px-4' width={164} height={26} alt="imageFooter" />
       <p className='text-sm text-gray-400 border-gray-400 border-l-[1.4px] px-4'>© 2022 Bitnovo. All rights reserved.</p>
     </footer>
