e352820c90ab5ab9f3d848b1949d289093a5b94b
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-29 19:45:03 -0300

    Add CustomButton and ButtonSelectPay components

---
+++ src/app/ui/components/Buttons.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Spinner } from './Spinner';
+
+interface CustomButtonProps {
+    text: string;
+    onClick: () => void;
+    disabled?: boolean;
+    isSelect?: boolean;
+    loading?: boolean
+}
+
+export const CustomButton = ({ text, onClick, disabled, loading }: CustomButtonProps) => {
+    return (
+        <button
+        type='submit'
+            disabled={disabled}
+            className={`${disabled ? 'bg-blue-300': 'bg-blue-600'} rounded-md 
+        text-white font-semibold text-base px-6 py-4 w-full`}
+            onClick={onClick}
+        >
+            {!loading ? text :  <Spinner/>}
+           
+        </button>
+    )
+}
+
+
+export const ButtonSelectPay = ({ text, onClick, disabled, isSelect }: CustomButtonProps) => {
+    return (
+        <button
+            type='button'
+            disabled={disabled}
+            className={` ${isSelect ? 'bg-blue-600 text-white ' : 'bg-gray-300 text-gray-500'} rounded-full 
+        text-base px-4 py-2`}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    )
+}
