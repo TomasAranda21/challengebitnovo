c84d1910f266727714b6c8b2fb90b60d251a95b2
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-29 19:45:19 -0300

    Add ErrorAlert component

---
+++ src/app/ui/components/ErrorAlert.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ErrorAlert = ({error} : {error:string}) => {
+  return (
+    <h3 className='text-red-500 text-center p-2 bg-red-100 mt-3 -mb-1.5 rounded-xl border-red-500 border'>{error}</h3>
+  )
+}
