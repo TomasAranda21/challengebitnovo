515d20c0a80b9f56663ee06a07c0da2f1b690cca
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-29 18:32:14 -0300

    Add CountdownTimer component

---
+++ src/app/ui/components/CountdownTimer.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { format, differenceInSeconds, parseISO, addSeconds, subSeconds } from 'date-fns';
+import Image from 'next/image';
+
+const CountdownTimer = ({ targetDate }: { targetDate: string }) => {
+    const [remainingTime, setRemainingTime] = useState(differenceInSeconds(parseISO(targetDate), new Date()));
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setRemainingTime(prevTime => {
+                if (prevTime <= 0) {
+                    clearInterval(intervalId);
+                    console.log('¡Se acabó el tiempo!');
+                    return 0;
+                }
+                return prevTime - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const formattedTime = format(remainingTime * 1000, 'mm:ss');
+    return (
+        <div className='flex items-center gap-2'>
+            <Image src='/timer.svg' width={24} height={24} alt="timer" />
+            <p>{formattedTime}</p>
+        </div>
+    );
+};
+
+export default CountdownTimer;
