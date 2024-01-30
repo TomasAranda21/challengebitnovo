356d1e337aaa0358432acdaa8741254ed0a0c163
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-30 12:29:10 -0300

    Add crypto interfaces

---
+++ src/app/lib/interfaces/cryptoInterfaces.ts
@@ -0,0 +1,11 @@
+export interface FormBuyCryptoProps {
+    amount: number;
+    concept: string;
+    crypto: string;
+}
+
+export interface CryptoOption {
+    symbol: string;
+    name: string;
+    image: string;
+}
