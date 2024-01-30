616e22714e5abc333b69a40a589e5fe9ac61f6b1
Tomas Aranda <tomasaranda2018@gmail.com>
2024-01-30 12:29:00 -0300

    Add component interfaces

---
+++ src/app/lib/interfaces/componentsInterfaces.tsx
@@ -0,0 +1,41 @@
+export interface TextProps {
+    isBold?: boolean;
+    text: string;
+}
+
+export interface CountdownTimerProps {
+    targetDate: string;
+    refresh: (e: boolean) => void;
+}
+
+
+export interface CopyToClipboardButtonProps {
+    text: string;
+    span?: string;
+    isBold?: boolean;
+    isAddress?: boolean;
+}
+
+
+export interface MySelectProps {
+    options: Option[];
+    onChange: (selectedOption: Option | null) => void;
+    label: string;
+    name: string;
+    valueOption: any;
+}
+
+export interface Option {
+    value: string;
+    label: string;
+    img: string;
+}
+
+
+export interface CustomButtonProps {
+    text: string;
+    onClick: () => void;
+    disabled?: boolean;
+    isSelect?: boolean;
+    loading?: boolean
+}
